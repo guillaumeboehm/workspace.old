7906e296cc682024a4f190e2f269bc97df6d59d4
Guillaume Boehm <Guillaume Boehm>
2016-10-08 23:54:29 +0200

    oih

---
+++ Unity/tuto1/Assets/EnnemiHealth.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var EnnemiHealth : int = 100;
+var Damage : int = 10;
+
+function OnCollisionEnter(col : Collision){
+    if(col.gameObject.tag == 'Bullet')
+        EnnemiHealth -= Damage;
+}
+
+function Update () {
+    if(EnnemiHealth <= 0)
+        Destroy(gameObject);
+}
